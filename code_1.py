f441a2d556705beaf6b9e40037b1bff51ebd34aa
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 00:04:59 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,245 @@
+# File 1
+
+for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}for (let i = 0; i < 10; i++) {
+    // JavaScript loop code goes here
+}
